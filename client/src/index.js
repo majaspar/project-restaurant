a9a6c901cbb00089163510deee5b95c347b7a25d
majaspar <130063598+majaspar@users.noreply.github.com>
2023-10-12 12:43:54 +0100

    add HashRouter

--- client/src/index.js
+++ client/src/index.js
@@ -14,6 +14,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
+
     <App />
   </Provider>
 );
