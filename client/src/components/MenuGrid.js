bd9ab14aa68600c742792a8eb3f57bd57357b827
majaspar <130063598+majaspar@users.noreply.github.com>
2023-10-24 13:26:28 +0100

    animations on menuscreen

--- client/src/components/MenuGrid.js
+++ client/src/components/MenuGrid.js
@@ -5,9 +5,20 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getAllDishes } from '../actions/dishActions';
 import Loading from './Loading'
 import Error from './Error'
+import { motion } from 'framer-motion'
 
 
 export default function MenuGrid({ menuSectionTitle, menuCategory, hash }) {
+
+    const fadeAnimVariants = {
+        hidden: {
+            opacity: 0
+        },
+        visible: (index: number) => ({
+            opacity: 1,
+            transition: { delay: 0.05 * index }
+        })
+    }
 
     const dispatch = useDispatch()
 
@@ -23,9 +34,17 @@ export default function MenuGrid({ menuSectionTitle, menuCategory, hash }) {
         <div className="menu__grid" id={hash}>
             <h2 className="menu__title center section-title mt3">{menuSectionTitle}</h2>
             {loading ? (<Loading />) : error ? (<Error message="There's been an error while loading the menu." />) : (
-                dishes.map((dish) => {
+                dishes.map((dish, index) => {
 
-                    return dish.category === menuCategory && <MenuGridItem key={dish._id} dish={dish} />
+                    return dish.category === menuCategory && <motion.li
+                        variants={fadeAnimVariants}
+                        initial="hidden"
+                        whileInView="visible"
+                        viewport={{
+                            once: true
+                        }}
+                        custom={index}
+                        key={dish._id}><MenuGridItem dish={dish} /></motion.li>
                 }
 
                 )
